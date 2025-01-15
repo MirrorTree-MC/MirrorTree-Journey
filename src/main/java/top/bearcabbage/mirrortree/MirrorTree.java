4be07ae19eb34adc218143290bc93031551030eb
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-01-16 05:04:12 +0800

    v0.2.0 Zzzz...

--- src/main/java/top/bearcabbage/mirrortree/MirrorTree.java
+++ src/main/java/top/bearcabbage/mirrortree/MirrorTree.java
@@ -1,10 +1,39 @@
 package top.bearcabbage.mirrortree;
 
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.Dynamic2CommandExceptionType;
+import com.mojang.brigadier.exceptions.Dynamic4CommandExceptionType;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.command.argument.Vec2ArgumentType;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.predicate.entity.LocationPredicate;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.scoreboard.AbstractTeam;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.ClickEvent;
 import net.minecraft.text.HoverEvent;
 import net.minecraft.text.Text;
@@ -12,18 +41,39 @@ import net.minecraft.text.Texts;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.*;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.minecraft.server.world.ServerWorld;
+import top.bearcabbage.lanterninstorm.LanternInStormAPI;
+import top.bearcabbage.lanterninstorm.lantern.BeginningLanternEntity;
+import top.bearcabbage.lanterninstorm.player.LiSPlayer;
 
-import java.awt.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static net.minecraft.state.property.Properties.WATERLOGGED;
+import static net.minecraft.world.World.OVERWORLD;
 
 public class MirrorTree implements ModInitializer {
 	public static final String MOD_ID = "mirrortree";
-
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static RegistryKey<World> bedroom = RegistryKey.of(RegistryKeys.WORLD, Identifier.of(MOD_ID, "bedroom"));
+	public static int bedroomX = 0;
+	public static int bedroomY = 100;
+	public static int bedroomZ = 0;
+
 
 	@Override
 	public void onInitialize() {
@@ -31,15 +81,30 @@ public class MirrorTree implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment)->MTCommand.registerCommands(dispatcher)); // 调用静态方法注册命令
+
+
+		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+			LanternInStormAPI.overrideRTPSpawnSetting();
+		});
+
 		EntitySleepEvents.START_SLEEPING.register((entity, sleepingLocation) -> {
 			entity.sendMessage(Texts.bracketed(Text.literal("[HHHor]离梦").styled((style) -> style.withColor(Formatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/wakeup")).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击在卧室中醒来"))))));
 		});
-		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
 			if (world.isClient) return ActionResult.SUCCESS;
-			if (world.getRegistryKey().getRegistry().equals(Identifier.of(MOD_ID,"bedroom"))) {
-				if (world.getBlockState(pos).getBlock() instanceof BedBlock) {
+			if (world.getRegistryKey().getValue().equals(Identifier.of(MOD_ID,"bedroom"))) {
+				if (world.getBlockState(hitResult.getBlockPos()).getBlock() instanceof BedBlock) {
 					// add Dreaming method
-					return ActionResult.SUCCESS;
+					if(((LiSPlayer)player).getLS().getRtpSpawn()==null) {
+						Dream.dreaming(world.getServer().getOverworld(), (ServerPlayerEntity) player);
+						return ActionResult.SUCCESS;
+					} else {
+						Dream.pos = ((LiSPlayer)player).getLS().getRtpSpawn();
+						((ServerPlayerEntity)player).teleport(world.getServer().getOverworld(), Dream.dreamingPos.get(player.getUuid()).toCenterPos().getX(), Dream.dreamingPos.get(player.getUuid()).toCenterPos().getY(), Dream.dreamingPos.get(player.getUuid()).toCenterPos().getZ(), 0,0);
+						Dream.dreamingPos.remove(player.getUuid());
+					}
 				}
 			}
             return ActionResult.PASS;
@@ -47,5 +112,99 @@ public class MirrorTree implements ModInitializer {
 
 	}
 
-	private static class Dream
+	private static class Dream{
+		public static final int MAX_RANGE = 50;
+		public static BlockPos pos;
+		static long lastTime = 0;
+
+		// 暂时没写储存
+		public static final Map<UUID,BlockPos> dreamingPos = Maps.newHashMap();
+
+		public static void dreaming(ServerWorld world, ServerPlayerEntity player) {
+			if (lastTime==0 || System.currentTimeMillis() - lastTime > 3000) {
+				lastTime = System.currentTimeMillis();
+				pos = getRandomPos(world);
+			}
+			LOGGER.info(pos.toString());
+			player.teleport(world, pos.toCenterPos().getX(), pos.toCenterPos().getY(), pos.toCenterPos().getZ(), 0,0);
+			LanternInStormAPI.setRTPSpawn(player, pos);
+			//	似乎找不到灯笼实体
+//			BeginningLanternEntity entity = world.getEntitiesByType(BeginningLanternEntity.BEGINNING_LANTERN, player.getBoundingBox().expand(2, 2, 2), lantern -> true).stream().findFirst().orElse(null);
+//			if (entity!=null) {
+//				String customName = entity.getCustomName().getLiteralString();
+//				String pattern = "入梦点\\[(.*?)\\]";
+//				Pattern r = Pattern.compile(pattern);
+//				Matcher m = r.matcher(customName);
+//				String extractedName = "";
+//				if (m.find()) {
+//					extractedName = m.group(1);
+//				}
+//				entity.setCustomName(Text.of("入梦点[" + extractedName + "," + player.getName().getString() + "]"));
+//			}
+		}
+
+		public static void redreaming(ServerWorld world, ServerPlayerEntity player) {
+			((LiSPlayer)player).getLS().setRtpSpawn(null);
+			List<BeginningLanternEntity> entities = (List<BeginningLanternEntity>) player.getServerWorld().getEntitiesByType(BeginningLanternEntity.BEGINNING_LANTERN, (entity)-> entity.getCustomName().getString().contains(player.getName().getString()));
+			if (entities.size()>0) {
+				entities.get(0).discard();
+			}
+			BlockPos pos1 = getRandomPos(world);
+			player.teleport(world, pos1.toCenterPos().getX(), pos1.toCenterPos().getY(), pos1.toCenterPos().getZ(), 0,0);
+			LanternInStormAPI.setRTPSpawn(player, pos1);
+		}
+
+		private static BlockPos getRandomPos(ServerWorld world) {
+            BlockPos blockPos = null;
+            BlockState blockState = null;
+            do {
+				Random random = new Random();
+                int x,z;
+                x = random.nextInt(-MAX_RANGE, MAX_RANGE);
+                z = random.nextInt(-MAX_RANGE, MAX_RANGE);
+                while (x*x+z*z>MAX_RANGE*MAX_RANGE){
+                    x = random.nextInt(-MAX_RANGE, MAX_RANGE);
+                    z = random.nextInt(-MAX_RANGE, MAX_RANGE);
+                }
+                blockPos = new BlockPos(x, world.getHeight(), z);
+//				LOGGER.info(blockPos.toString());
+                while(world.getBlockState(blockPos).isAir()){
+                    blockPos = blockPos.down();
+                }
+                blockState = world.getBlockState(blockPos);
+            }
+			while(!blockState.get(WATERLOGGED) || blockState.isLiquid() || blockState.isIn(BlockTags.FIRE) || blockState.isIn(BlockTags.LEAVES));
+			return blockPos;
+		}
+
+
+	}
+
+	private static class MTCommand {
+
+		public static void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {
+			dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal("wakeup")
+					.executes(context -> {
+						ServerPlayerEntity player = context.getSource().getPlayer();
+						if (player==null) return 0;
+						if (!player.isSleeping()) return 0;
+						player.wakeUp();
+						Dream.dreamingPos.put(player.getUuid(), player.getBlockPos());
+						ServerWorld bedroom = player.getServer().getWorld(MirrorTree.bedroom);
+						player.teleport(bedroom, bedroomX, bedroomY, bedroomZ, 0,0);
+						return 0;
+					})
+			);
+			dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal("redream")
+					.requires(source -> source.hasPermissionLevel(2))
+					.executes(context -> {
+						ServerPlayerEntity player = context.getSource().getPlayer();
+						if (player==null) return 0;
+						Dream.redreaming(player.getServer().getOverworld(), player);
+						return 0;
+					})
+			);
+		}
+	}
+
 }
