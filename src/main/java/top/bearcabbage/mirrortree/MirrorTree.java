edf3607ada322ec5137a339ff1246158a2b3db06
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-01-16 23:12:05 +0800

    v1.0.0 - 将老玩家睡觉点的数据关服写入文件

--- src/main/java/top/bearcabbage/mirrortree/MirrorTree.java
+++ src/main/java/top/bearcabbage/mirrortree/MirrorTree.java
@@ -1,38 +1,26 @@
 package top.bearcabbage.mirrortree;
 
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.BoolArgumentType;
-import com.mojang.brigadier.arguments.FloatArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.exceptions.Dynamic2CommandExceptionType;
-import com.mojang.brigadier.exceptions.Dynamic4CommandExceptionType;
-import com.sun.jdi.connect.Connector;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.command.argument.Vec2ArgumentType;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.predicate.entity.LocationPredicate;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.BlockTags;
-import net.minecraft.scoreboard.AbstractTeam;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.ClickEvent;
@@ -43,9 +31,8 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.*;
+import net.minecraft.world.GameMode;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldAccess;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.minecraft.server.world.ServerWorld;
@@ -53,30 +40,26 @@ import top.bearcabbage.lanterninstorm.LanternInStormAPI;
 import top.bearcabbage.lanterninstorm.lantern.BeginningLanternEntity;
 import top.bearcabbage.lanterninstorm.player.LiSPlayer;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.state.property.Properties.WATERLOGGED;
-import static net.minecraft.world.World.OVERWORLD;
 
 public class MirrorTree implements ModInitializer {
 	public static final String MOD_ID = "mirrortree";
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	public static final Config dream = new Config(FabricLoader.getInstance().getConfigDir().resolve(MOD_ID).resolve("dream_data.json"));
 	public static RegistryKey<World> bedroom = RegistryKey.of(RegistryKeys.WORLD, Identifier.of(MOD_ID, "bedroom"));
 	public static int bedroomX = 0;
-	public static int bedroomY = 100;
+	public static int bedroomY = 80;
 	public static int bedroomZ = 0;
 
 
@@ -85,31 +68,53 @@ public class MirrorTree implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-
-
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment)->MTCommand.registerCommands(dispatcher)); // 调用静态方法注册命令
-
 
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
 			LanternInStormAPI.overrideRTPSpawnSetting();
 			LanternInStormAPI.addSafeWorld(bedroom);
+			Dream.dreamingPos.clear();
+            try {
+				dream.getAll(Map.class).forEach((uuid, pos) -> {
+					ArrayList<Double> posList = (ArrayList<Double>) pos;
+					double[] posArray = new double[3];
+					for (int i = 0; i < 3; i++) {
+						posArray[i] = posList.get(i);
+					}
+					Dream.dreamingPos.put(UUID.fromString((String) uuid), posArray);
+				});
+            } catch (Exception e) {
+				LOGGER.error(e.getMessage());
+            }
+        });
+
+		ServerLifecycleEvents.SERVER_STOPPING.register(server -> {
+			Dream.dreamingPos.forEach((uuid, pos) -> dream.set(String.valueOf(uuid), pos));
+			dream.save();
 		});
 
 		EntitySleepEvents.START_SLEEPING.register((entity, sleepingLocation) -> {
-			entity.sendMessage(Texts.bracketed(Text.literal("[HHHor]离梦").styled((style) -> style.withColor(Formatting.GREEN).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/wakeup")).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击在卧室中醒来"))))));
+			entity.sendMessage(Texts.bracketed(Text.literal("=======§l[醒来走走]§r=======").styled((style) -> style.withColor(Formatting.LIGHT_PURPLE).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/wakeup")).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击回到卧室（出生点大厅）维度"))))));
 		});
 
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
 			if (world.isClient) return ActionResult.SUCCESS;
 			if (world.getRegistryKey().getValue().equals(Identifier.of(MOD_ID,"bedroom"))) {
 				if (world.getBlockState(hitResult.getBlockPos()).getBlock() instanceof BedBlock) {
-//					Dream.dreaming(player.getServer().getOverworld(), (ServerPlayerEntity) player);
 					Dream.queueDreamingTask(player.getServer().getOverworld(), (ServerPlayerEntity) player);
+					return ActionResult.SUCCESS;
 				}
 			}
             return ActionResult.PASS;
         });
+
+		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+			ServerPlayerEntity player = handler.player;
+			if (!player.getServerWorld().getRegistryKey().equals(bedroom) || LanternInStormAPI.getRTPSpawn(player) == null) {
+				player.teleport(player.getServer().getWorld(bedroom), bedroomX, bedroomY, bedroomZ, 0, 0);
+				player.changeGameMode(GameMode.ADVENTURE);
+			}
+		});
 
 	}
 
@@ -159,10 +164,8 @@ public class MirrorTree implements ModInitializer {
 			});
 		}
 
-
-
-		// 暂时没写储存
-		public static final Map<UUID,BlockPos> dreamingPos = Maps.newHashMap();
+		// 正常关服时写文件储存数据
+		public static final Map<UUID, double[]> dreamingPos = Maps.newHashMap();
 
 		public static void dreaming(ServerWorld world, ServerPlayerEntity player) {
 			if(((LiSPlayer)player).getLS().getRtpSpawn()==null) {
@@ -178,12 +181,13 @@ public class MirrorTree implements ModInitializer {
 				pos = ((LiSPlayer) player).getLS().getRtpSpawn();
 				lastTime = System.currentTimeMillis();
 				if (dreamingPos.containsKey(player.getUuid())) {
-					((ServerPlayerEntity) player).teleport(world.getServer().getOverworld(), dreamingPos.get(player.getUuid()).toCenterPos().getX(), dreamingPos.get(player.getUuid()).toCenterPos().getY(), dreamingPos.get(player.getUuid()).toCenterPos().getZ(), 0, 0);
+					((ServerPlayerEntity) player).teleport(world.getServer().getOverworld(), dreamingPos.get(player.getUuid())[0]+0.5, dreamingPos.get(player.getUuid())[1]+0.5, dreamingPos.get(player.getUuid())[2]+0.5, 0, 0);
 					dreamingPos.remove(player.getUuid());
 				} else {
 					((ServerPlayerEntity) player).teleport(world.getServer().getOverworld(), pos.toCenterPos().getX(), pos.toCenterPos().getY(), pos.toCenterPos().getZ(), 0, 0);
 				}
 			}
+			player.changeGameMode(GameMode.SURVIVAL);
 		}
 
 		public static void redreaming(ServerWorld world, ServerPlayerEntity player) {
@@ -232,9 +236,11 @@ public class MirrorTree implements ModInitializer {
 						if (player==null) return 0;
 						if (!player.isSleeping()) return 0;
 						player.wakeUp();
-						Dream.dreamingPos.put(player.getUuid(), player.getBlockPos());
+						double[] pos = {player.getBlockPos().getX(), player.getBlockPos().getY(), player.getBlockPos().getZ()};
+						Dream.dreamingPos.put(player.getUuid(), pos);
 						ServerWorld bedroom = player.getServer().getWorld(MirrorTree.bedroom);
 						player.teleport(bedroom, bedroomX, bedroomY, bedroomZ, 0,0);
+						player.changeGameMode(GameMode.ADVENTURE);
 						return 0;
 					})
 			);
@@ -252,6 +258,61 @@ public class MirrorTree implements ModInitializer {
 					})
 			);
 		}
+	}
+
+	public static class Config {
+		private final Path filePath;
+		private JsonObject jsonObject;
+		private final Gson gson;
+
+		public Config(Path filePath) {
+			this.filePath = filePath;
+			this.gson = new GsonBuilder().setPrettyPrinting().create();
+			try {
+				if (Files.notExists(filePath.getParent())) {
+					Files.createDirectories(filePath.getParent());
+				}
+				if (Files.notExists(filePath)) {
+					Files.createFile(filePath);
+					try (FileWriter writer = new FileWriter(filePath.toFile())) {
+						writer.write("{}");
+					}
+				}
+
+			} catch (IOException e) {
+				LOGGER.error(e.toString());
+			}
+			load();
+		}
+
+		public void load() {
+			try (FileReader reader = new FileReader(filePath.toFile())) {
+				this.jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+			} catch (IOException e) {
+				this.jsonObject = new JsonObject();
+			}
+		}
+
+		public void save() {
+			try (FileWriter writer = new FileWriter(filePath.toFile())) {
+				gson.toJson(jsonObject, writer);
+			} catch (IOException e) {
+				LOGGER.error(e.toString());
+			}
+		}
+
+		public void set(String key, Object value) {
+			jsonObject.add(key, gson.toJsonTree(value));
+		}
+
+		public <T> T get(String key, Class<T> clazz) {
+			return gson.fromJson(jsonObject.get(key), clazz);
+		}
+
+		public <T> T getAll(Class<T> clazz) {
+			return gson.fromJson(jsonObject, clazz);
+		}
+
 	}
 
 }
