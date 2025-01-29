ba4a0d95ba40a1f978e033806a4239fc3085ebec
Bai Yuxuan <xiaoxiongbaicai@outlook.com>
2025-01-29 19:57:49 +0800

    v2.0.7 - re - 都work了 - 修改了「醒来」提示，删去了非法字符

--- src/main/java/top/bearcabbage/mirrortree/MTClient.java
+++ src/main/java/top/bearcabbage/mirrortree/MTClient.java
@@ -17,13 +17,13 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.mojang.text2speech.Narrator.LOGGER;
+import static top.bearcabbage.mirrortree.MirrorTree.MOD_ID;
 
 public class MTClient {
     public static final Map<String, String> modsServer = new HashMap<>();
@@ -42,6 +42,10 @@ public class MTClient {
         String timestamp = LocalDateTime.now().format(formatter);
         File shot = new File(SCREENSHOT_PATH.resolve(playerName + "-" + timestamp + ".png").toUri());
         try {
+            if(!SCREENSHOT_PATH.toFile().exists()) {
+                SCREENSHOT_PATH.toFile().mkdir();
+            }
+            LOGGER.info("[MirrorTree]Taking screenshot");
             ScreenshotRecorder.takeScreenshot(MinecraftClient.getInstance().getFramebuffer()).writeTo(shot);
         } catch (Exception e) {
             LOGGER.error("[MirrorTree]" + e);
@@ -63,6 +67,7 @@ public class MTClient {
         MTClient.modsURL.putAll(config1.getOrDefault("urls", new HashMap<>()));
         isUpdate_modsServer = isUpdate_modsURL = true;
         MTClient.updateMods();
+
     }
 
     public static void updateMods() {
@@ -70,10 +75,14 @@ public class MTClient {
             return;
         }
         Set<String> mods_needUpdate = new HashSet<>();
+        Map<String, String> modsOutdated = new HashMap<>();
         modsServer.forEach((modid, version) -> {
             Mod mod = ModMenu.MODS.values().stream().filter(m -> m.getId().equals(modid)).findFirst().orElse(null);
-            if (mod == null || !mod.getVersion().equals(version)) {
+            if (mod == null || mod.getVersion().compareTo(version) < 0) {
                 mods_needUpdate.add(modid);
+                if (mod != null && !modid.equals(MOD_ID)) {
+                    modsOutdated.put(modid, mod.getVersion());
+                }
             }
         });
         if (mods_needUpdate.isEmpty()) return;
@@ -81,14 +90,26 @@ public class MTClient {
         Path modsDir = MinecraftClient.getInstance().runDirectory.toPath().resolve("mods");
         mods_needUpdate.forEach(mod -> {
             try {
-                downloadFile(mod.concat(".jar"), modsURL.get(mod), modsDir.toString());
+                downloadFile(mod.concat("-").concat(modsServer.get(mod)).concat(".jar"), modsURL.get(mod), modsDir.toString());
                 LOGGER.info("Downloaded " + mod);
+                if (modsOutdated.containsKey(mod)) {
+                    File modOld = modsDir.resolve(mod.concat("-").concat(modsOutdated.get(mod)).concat(".jar")).toFile();
+                    if (modOld.exists()) {
+                        modOld.delete();
+                        LOGGER.info("Deleted old " + modOld.getName());
+                    }
+                    modOld = modsDir.resolve(mod.concat(".jar")).toFile();
+                    if (modOld.exists()) {
+                        modOld.delete();
+                        LOGGER.info("Deleted old " + modOld.getName());
+                    }
+                }
                 updatedMods.put(mod, modsServer.get(mod));
             } catch (Exception e) {
                 LOGGER.error(e.toString());
             }
         });
-        MinecraftClient.getInstance().close();
+        MinecraftClient.getInstance().stop();
         throw new RuntimeException("[MirrorTree]客户端更新完成，请重启客户端");
     }
 
--- src/main/java/top/bearcabbage/mirrortree/MirrorTree.java
+++ src/main/java/top/bearcabbage/mirrortree/MirrorTree.java
@@ -106,7 +106,7 @@ public class MirrorTree implements ModInitializer {
 		});
 
 		EntitySleepEvents.START_SLEEPING.register((entity, sleepingLocation) -> {
-			entity.sendMessage(Texts.bracketed(Text.literal("=======§l[醒来走走]§r=======").styled((style) -> style.withColor(Formatting.LIGHT_PURPLE).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/wakeup")).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击回到卧室（出生点大厅）维度"))))));
+			entity.sendMessage(Texts.bracketed(Text.literal("=======[醒来走走]=======").formatted(Formatting.BOLD).styled((style) -> style.withColor(Formatting.LIGHT_PURPLE).withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/wakeup")).withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal("点击回到卧室（出生点大厅）维度"))))));
 		});
 
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
